de3c307585b36e07fd435b3c0e11f0979865e601
Wag210095 <WAG210095@spengergasse.at>
2022-10-07 16:36:35 +0200

    Übung Properties

---
+++ Uebung.Properties/Uebung.Properties/Lehrer.cs
@@ -0,0 +1,37 @@
+﻿namespace ExProperties.App
+{
+    internal class Lehrer
+    {
+        private decimal? _bG;
+        private decimal? _nG;
+
+        public string? Vorname { get;  set; } = string.Empty;
+        public string? Zuname { get;  set; }
+        public decimal? Bruttogehalt
+        {
+            get { return _bG; }
+            set
+            {
+                if (_bG != null)
+                {
+                    _bG = value;
+                }
+            }
+        }
+        public decimal? Nettogehalt
+        {
+            get { return _nG * 0.8M; } 
+
+            set
+            {
+                if (_bG == null)
+                {
+
+                    _nG = 0;
+
+                }
+            } 
+        }
+        public string? Kuerzel { get;  set; } //keine Ahnung
+    }
+}
---
+++ Uebung.Properties/Uebung.Properties/Rechteck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung.Properties
+{
+    public class Rechteck
+    {
+        private int _laenge;
+        private int _breite;
+        
+       
+        public int Laenge
+        {
+            get { return _laenge; }
+
+            set
+            {
+                if (_laenge < 0)
+                {
+                    throw new ArgumentException("ungültige Länge!");
+                }
+            }
+        }
+        public int Breite {  get { return _breite; }
+
+            set
+            {
+                if (_breite < 0)
+                {
+                    throw new ArgumentException("ungültige Länge!");
+                }
+            }
+        }
+        public int Flaeche {
+
+         
+
+            get { return _laenge*_breite; }
+
+
+        }
+
+    }
+
+}
